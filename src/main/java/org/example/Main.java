fef6e7a24692cdfd1fa8c6ac358756f9a6a63ec0
Igor Belianchikov <igorbelianchikov@Air-Igor.lan>
2024-11-24 22:51:23 +0100

    Some new code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello another world!");
         System.out.println("Bye-bye world!");
+        System.out.println("some new text");
     }
 }
